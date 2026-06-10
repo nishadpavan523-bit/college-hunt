c93cae590f943dfdc05ae24705c917acf79da88d
Pavan Nishad <nishadpavan523@gmail.com>
2026-06-10 20:02:15 +0530

    1

--- project/script.js
+++ project/script.js
@@ -3360,10 +3360,6 @@ function handleLogin() {
 function handleDemoLogin() {
   goToStreamPage("Demo Student");
 }
-function handleDemoLogin() {
-   // your code
-}
-window.handleDemoLogin = handleDemoLogin;
 function showRegister() {
   showAlert("Registration coming soon! Use Demo Login to explore.", "success");
 }
@@ -4243,7 +4239,3 @@ Object.assign(window, {
   toggleChip, setRecommend, submitFeedback, resetFeedback,
   jumpToSection
 });
-window.handleLogin = handleLogin;
-window.handleDemoLogin = handleDemoLogin;
-window.showRegister = showRegister;
-window.togglePassword = togglePassword;
